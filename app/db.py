786ce11139e8c156e71e7ccddf46440395135c66
Merhawi <45622655+mtm-007@users.noreply.github.com>
2025-03-19 02:53:44 +0000

    fix typo

--- app/db.py
+++ app/db.py
@@ -9,10 +9,10 @@ tz = ZoneInfo("America/Los_Angeles")
 
 def get_db_connection():
     return psycopg2.connect(
-        host = os.getenv("POSTGRES_HOST", "postgres"),
-        database = os.getenv("POSTGRES_DB", "course_assitant"),
-        user = os.getenv("POSTGRES_USER", "user_name"),
-        password = os.getenv("POSTGRES_PASSWORD", "sample_password"),
+        host=os.getenv("POSTGRES_HOST", "postgres"),
+        database=os.getenv("POSTGRES_DB", "course_assitant"),
+        user=os.getenv("POSTGRES_USER", "user_name"),
+        password=os.getenv("POSTGRES_PASSWORD", "sample_password"),
     )
 
 
@@ -24,7 +24,7 @@ def init_db():
             cur.execute("DROP TABLE IF EXISTS conversations")
 
             cur.execute("""
-                CREATE TABLE conversations(
+                CREATE TABLE conversations (
                     id TEXT PRIMARY KEY,
                     question TEXT NOT NULL,
                     answer TEXT NOT NULL,
@@ -44,7 +44,7 @@ def init_db():
                 )
             """)
             cur.execute("""
-                CREATE TABLE feedback(
+                CREATE TABLE feedback (
                     id SERIAL PRIMARY KEY,
                     conversation_id TEXT REFERENCES conversations(id),
                     feedback INTEGER NOT NULL,
@@ -56,7 +56,7 @@ def init_db():
         conn.close()
 
 
-def save_conversations(conversation_id, question, answer_data, course, timestamp=None):
+def save_conversation(conversation_id, question, answer_data, course, timestamp=None):
     if timestamp is None:
         timestamp = datetime.now(tz)
 
@@ -65,12 +65,12 @@ def save_conversations(conversation_id, question, answer_data, course, timestamp
         with conn.cursor() as cur:
             cur.execute(
                 """
-                INSERT INTO conversations(
-                id, question, answer, course, model_used, response_time, relevance,
+                INSERT INTO conversations
+                (id, question, answer, course, model_used, response_time, relevance,
                 relevance_explanation, prompt_tokens, completion_tokens, total_tokens,
                 eval_prompt_tokens, eval_completion_tokens, eval_total_tokens, openai_cost, timestamp)
                 VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, COALESCE(%s, CURRENT_TIMESTAMP))
-                """,
+            """,
                 (
                     conversation_id,
                     question,
@@ -103,7 +103,7 @@ def save_feedback(conversation_id, feedback, timestamp=None):
     try:
         with conn.cursor() as cur:
             cur.execute(
-                "INSERT INTO feedback(conversation_id, feedback, timestamp) VALUES( %s, %s, COALESCE(%s, CURRENT_TIMESTAMP))",
+                "INSERT INTO feedback (conversation_id, feedback, timestamp) VALUES( %s, %s, COALESCE(%s, CURRENT_TIMESTAMP))",
                 (conversation_id, feedback, timestamp),
             )
         conn.commit()
@@ -114,14 +114,14 @@ def save_feedback(conversation_id, feedback, timestamp=None):
 def get_recent_conversations(limit=5, relevance=None):
     conn = get_db_connection()
     try:
-        with conn.cursor(cursor_factory = DictCursor) as cur:
+        with conn.cursor(cursor_factory=DictCursor) as cur:
             query = """
                 SELECT c.*, f.feedback
                 FROM conversations c
                 LEFT JOIN feedback f ON c.id = f.conversation_id
-                """
+            """
             if relevance:
-                query += f" WHERE c.relevance = '{relevance}' "
+                query += f" WHERE c.relevance = '{relevance}'"
             query += " ORDER BY c.timestamp DESC LIMIT %s"
 
             cur.execute(query, (limit,))
@@ -134,11 +134,11 @@ def get_feedback_stats():
     conn = get_db_connection()
 
     try:
-        with conn.cursor(cursor_factory =DictCursor) as cur:
+        with conn.cursor(cursor_factory=DictCursor) as cur:
             cur.execute("""
                 SELECT
-                    SUM(CASE WHEN feedback > 0 THEN 1 ELSE 0 END) as thumps_up,
-                    SUM(CASE WHEN feedback < 0 THEN 1 ELSE 0 END) as thumps_down
+                    SUM(CASE WHEN feedback > 0 THEN 1 ELSE 0 END) as thumbs_up,
+                    SUM(CASE WHEN feedback < 0 THEN 1 ELSE 0 END) as thumbs_down
                 FROM feedback
             """)
             return cur.fetchone()
