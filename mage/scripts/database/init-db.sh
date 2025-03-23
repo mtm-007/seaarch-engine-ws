572b42152244c12352eea8370c3f0f8545363652
Merhawi <45622655+mtm-007@users.noreply.github.com>
2025-03-23 21:24:37 +0000

    mage for orchestration setup

---
+++ mage/scripts/database/init-db.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# set -e is removed to allow the script to continue executing after an error.
+
+create_db_if_not_exists() {
+    local dbname=$1
+    PGPASSWORD=$POSTGRES_PASSWORD psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" --list | grep -qw $dbname
+    if [ $? -ne 0 ]; then
+        echo "Database $dbname not found, creating..."
+        PGPASSWORD=$POSTGRES_PASSWORD psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" -c "CREATE DATABASE $dbname;"
+    else
+        echo "Database $dbname already exists, skipping creation."
+    fi
+}
+
+#create the main application database.
+create_db_if_not_exists "$POSTGRES_DB"
+
+# Assuming the role exists; otherwise, you'd include CREATE ROLE commands as needed.
+# Grant all privileges on the databases to the specified user.
+PGPASSWORD=$POSTGRES_PASSWORD psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "postgres" <<-EOSQL
+    GRANT ALL PRIVILEGES ON DATABASE "$POSTGRES_DB" TO "$POSTGRES_USER";
+    GRANT ALL PRIVILEGES ON DATABASE "$EXPERIMENTS_DB" TO "$POSTGRES_USER";
+EOSQL
